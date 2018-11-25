d7941d47d9a3931c460c1f17216b5c0e8c280e35
William Chiu <williamchiu@Williamde-MacBook-Pro.local>
2018-11-26 00:05:53 +0800

    parser with argument

--- parser.py
+++ parser.py
@@ -4,20 +4,53 @@ import matplotlib
 import matplotlib.pyplot as plt    # pip install matplotlib
 import pandas as pd                # pip install pandas
 import time
+import sys
+import getopt
+
+
+
+target=""
+
+index=sys.argv[1]
+if index=="twii":
+    target="^TWII"
+elif index=="dji":
+    target="^DJI"
+else:
+    target=index+".TW"
+
+
+
+kop=sys.argv[2]
+if kop=="close":
+    ktype="Close"
+elif kop=="open":
+    ktype="Open"
+elif kop=="high":
+    ktype="High"
+elif kop=="low":
+    ktype=="Low"
+
+print ("fetching data for: "+target+" with "+kop+" type")
+
+
+
+
+
 
 now=(time.strftime("%Y-%m-%d")) 
 
 #print time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) 
 
 
-data = data.DataReader("^TWII", "yahoo", "2000-01-01", now)
-c = data['Close']
+data = data.DataReader(target, "yahoo", "2000-01-01", now)
+c = data[ktype]
 c.plot()
 plt.show()
 
 
+
+
 dit = {"price": c}
-
 df = pd.DataFrame(dit)
-
 df.to_csv("price.csv", columns=["price"])
